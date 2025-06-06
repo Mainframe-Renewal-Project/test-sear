79bdbb0307906a237b1830d00da9536f283b1db2
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-06 17:22:47 +0200

    Initial work on test shell script

--- test.sh
+++ test.sh
@@ -1,1 +1,17 @@
+# This shell script runs the tests and cleans up after them in case they failed
+
 set -e
+
+function run_test {
+
+}
+
+function clean_up {
+    /bin/tsocmd "deluser SEART1"
+}
+
+# Run test
+run_test
+
+# Run clean up function
+clean_up
