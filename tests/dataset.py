165a6eeb064b8b74583f0880c2e99d7d2834e27d
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-06 17:38:41 +0200

    Dataset profile test case

---
+++ tests/dataset.py
@@ -0,0 +1,36 @@
+# Import SEAR
+from sear import sear
+
+add_result = sear(
+        {
+        "operation": "add", 
+        "admin_type": "data-set", 
+        "data_set": "SEARTEST.**.**",
+        "traits": {
+            "base:installation_data": "DATASET PROFILE GENERATED DURING SEAR TESTING, NOT IMPORTANT",
+        },
+        },
+    )
+
+print(add_result.result)
+
+extract_result = sear(
+        {
+        "operation": "extract",
+        "admin_type": "data-set", 
+        "data_set": "SEARTEST.**.**",
+        },
+        True
+    )
+
+print(extract_result.result)
+
+delete_result = sear(
+        {
+        "operation": "delete",
+        "admin_type": "data-set", 
+        "data_set": "SEARTEST.**.**",
+        },
+    )
+
+print(delete_result.result)
