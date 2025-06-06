41659ed431dfc4db965d1c3293cdcd697dcc416f
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-06 19:59:48 +0200

    Added pytest to group tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,17 @@ def delete_user():
 def create_user(delete_user):
     run_tso_command(f"adduser {delete_user} DATA('USER GENERATED DURING SEAR TESTING, NOT IMPORTANT')")
     yield delete_user
+
+@pytest.fixture
+def delete_group():
+    groupid=f"SEAR{secrets.token_hex(2)}"
+    yield groupid
+    try:
+        run_tso_command(f"delgroup {groupid}")
+    except:  # noqa: E722
+        pass
+
+@pytest.fixture
+def create_group(delete_group):
+    run_tso_command(f"addgroup {delete_group} DATA('GROUP GENERATED DURING SEAR TESTING, NOT IMPORTANT')")
+    yield delete_group
--- tests/test_group.py
+++ tests/test_group.py
@@ -1,48 +1,50 @@
 # Import SEAR
 from sear import sear
 
-add_result = sear(
-        {
-        "operation": "add", 
-        "admin_type": "group", 
-        "group": "SEARG1",
-        "traits": {
-            "base:installation_data": "GROUP GENERATED DURING SEAR TESTING, NOT IMPORTANT",
-        },
-        },
-    )
+from helper import successful_return_codes
 
-print(add_result.result)
+def test_add_group(delete_group):
+    add_result = sear(
+            {
+            "operation": "add", 
+            "admin_type": "group", 
+            "group": delete_group,
+            "traits": {
+                "base:installation_data": "GROUP GENERATED DURING SEAR TESTING, NOT IMPORTANT",
+            },
+            },
+        )
+    assert add_result.result["return_codes"] == successful_return_codes
 
-extract_result = sear(
-        {
-        "operation": "extract",
-        "admin_type": "group",
-        "group": "SEARG1"
-        },
-    )
+def test_extract_group(create_group):
+    extract_result = sear(
+            {
+            "operation": "extract",
+            "admin_type": "group",
+            "group": create_group
+            },
+        )
+    assert extract_result.result["return_codes"] == successful_return_codes
 
-print(extract_result.result)
+def test_alter_group(create_group):
+    alter_result = sear(
+            {
+            "operation": "alter", 
+            "admin_type": "group", 
+            "group": create_group,
+            "traits": {
+                "omvs:auto_gid": True,
+            },
+            },
+        )
+    assert alter_result.result["return_codes"] == successful_return_codes
 
-alter_result = sear(
-        {
-        "operation": "alter", 
-        "admin_type": "group", 
-        "group": "SEARG1",
-        "traits": {
-            "omvs:auto_gid": True,
-        },
-        },
-    )
-
-print(alter_result.result)
-
-delete_result = sear(
-        {
-        "operation": "delete",
-        "admin_type": "group",
-        "group": "SEARG1",
-        },
-    )
-
-print(delete_result.result)
+def test_delete_group(create_group):
+    delete_result = sear(
+            {
+            "operation": "delete",
+            "admin_type": "group",
+            "group": create_group,
+            },
+        )
+    assert delete_result.result["return_codes"] == successful_return_codes
