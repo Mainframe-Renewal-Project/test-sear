d3d57cd077dcc071e4acf97462f6ff648c2dc190
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-06 17:39:00 +0200

    Resource profile test case

---
+++ tests/resource.py
@@ -0,0 +1,53 @@
+# Import SEAR
+from sear import sear
+
+add_result = sear(
+        {
+        "operation": "add", 
+        "admin_type": "resource", 
+        "resource": "sear.test",
+        "class": "appl",
+        "traits": {
+            "base:installation_data": "RESOURCE PROFILE GENERATED DURING SEAR TESTING, NOT IMPORTANT",
+        },
+        },
+    )
+
+print(add_result.result)
+
+extract_result = sear(
+        {
+        "operation": "extract",
+        "admin_type": "resource",
+        "resource": "sear.test",
+        "class": "appl",
+        },
+        True
+    )
+
+print(extract_result.result)
+
+alter_result = sear(
+        {
+        "operation": "alter", 
+        "admin_type": "resource", 
+        "resource": "sear.test",
+        "class": "appl",
+        "traits": {
+            "base:universal_access": "READ",
+        },
+        },
+    )
+
+print(alter_result.result)
+
+delete_result = sear(
+        {
+        "operation": "delete",
+        "admin_type": "resource",
+        "resource": "sear.test",
+        "class": "appl",
+        },
+    )
+
+print(delete_result.result)
