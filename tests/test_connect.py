f71f16a55c72d2550bde7b7a9ecff1df0e08bca7
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-07 19:02:13 +0200

    fixed several issues in the connect test case

--- tests/test_connect.py
+++ tests/test_connect.py
@@ -10,7 +10,7 @@ from tests.conftest import run_tso_command
 def test_add_connect(create_user):
     add_result = sear(
             {
-            "operation": "add", 
+            "operation": "alter", 
             "admin_type": "group-connection", 
             "userid": create_user,
             "group": "SEARDUMY",
@@ -20,5 +20,5 @@ def test_add_connect(create_user):
             },
         )
     #run_tso_command(f"REMOVE {create_user} GROUP(SEARDUMY)")
-    assert "errors" not in str(delete_result.result)
+    assert "errors" not in str(add_result.result)
     assert add_result.result["return_codes"] == successful_return_codes
