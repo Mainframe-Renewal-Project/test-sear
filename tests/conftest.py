38aeb35d329fa628b24ccacbeae7147da18305b6
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-07 17:20:42 +0200

    improved fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -53,7 +53,7 @@ def delete_dataset():
 
 @pytest.fixture
 def create_dataset(delete_dataset):
-    run_tso_command(f"ADDSD ({delete_dataset}) DATA('DATASET PROFILE GENERATED DURING SEAR TESTING, NOT IMPORTANT') GENERIC")  # noqa: E501
+    run_tso_command(f"ADDSD ({delete_dataset}) DATA('DATASET PROFILE GENERATED DURING SEAR TESTING, NOT IMPORTANT') GENERIC OWNER(SYS1)")  # noqa: E501
     run_tso_command("SETROPTS GENERIC(DATASET) REFRESH")
     yield delete_dataset
 
@@ -70,7 +70,7 @@ def delete_resource():
 @pytest.fixture
 def create_resource(delete_resource):
     profile_name, class_name = delete_resource
-    run_tso_command(f"RDEFINE {class_name} ({profile_name}) DATA('RESOURCE PROFILE GENERATED DURING SEAR TESTING, NOT IMPORTANT')")  # noqa: E501
+    run_tso_command(f"RDEFINE {class_name} ({profile_name}) DATA('RESOURCE PROFILE GENERATED DURING SEAR TESTING, NOT IMPORTANT') GENERIC OWNER(SYS1)")  # noqa: E501
     run_tso_command(f"SETROPTS GENERIC({class_name}) REFRESH")
     run_tso_command(f"SETROPTS RACLIST({class_name}) REFRESH")
     yield profile_name, class_name
