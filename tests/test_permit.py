2c84147c41785fb142f5640d761c8d4d2f1f9135
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-07 21:50:01 +0200

    added test case for dataset permit

---
+++ tests/test_permit.py
@@ -0,0 +1,22 @@
+
+from helper import successful_return_codes
+
+# Import SEAR
+from sear import sear
+
+
+def test_add_dataset_permit(create_user, create_dataset):
+    add_result = sear(
+            {
+            "operation": "alter", 
+            "admin_type": "permission", 
+            "data_set": create_dataset,
+            "userid": create_user,
+            "generic": True,
+            "traits": {
+                "base:access": "READ",
+            },
+            },
+        )
+    assert "errors" not in str(add_result.result)
+    assert add_result.result["return_codes"] == successful_return_codes
