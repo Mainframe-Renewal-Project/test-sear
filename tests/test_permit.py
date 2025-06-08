0ecb12f9a19ad4b1e9e931e8f859e8b43224fd04
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-08 14:49:38 +0200

    Added resource permit test

--- tests/test_permit.py
+++ tests/test_permit.py
@@ -21,3 +21,22 @@ def test_add_dataset_permit(create_user, create_dataset):
         )
     assert "errors" not in str(add_result.result)
     assert add_result.result["return_codes"] == successful_return_codes
+
+def test_add_resource_permit(create_user, create_resource):
+    """This test is supposed to succeed"""
+    profile_name, class_name = create_resource
+    add_result = sear(
+            {
+            "operation": "alter", 
+            "admin_type": "permission", 
+            "resource": profile_name,
+            "class": class_name,
+            "userid": create_user,
+            "generic": True,
+            "traits": {
+                "base:access": "READ",
+            },
+            },
+        )
+    assert "errors" not in str(add_result.result)
+    assert add_result.result["return_codes"] == successful_return_codes
