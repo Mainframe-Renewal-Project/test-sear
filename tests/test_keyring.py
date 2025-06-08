cc637486a5b35981a0481072c5767cce02228911
Emma S. <64806882+EmmasBox@users.noreply.github.com>
2025-06-08 15:00:26 +0200

    Improved keyring test cases and added new ones

--- tests/test_keyring.py
+++ tests/test_keyring.py
@@ -6,6 +6,7 @@ from sear import sear
 
 
 def test_extract_keyring_not_found():
+    """This test is supposed to fail"""
     not_found_result = sear(
         {
         "operation": "extract", 
@@ -14,9 +15,35 @@ def test_extract_keyring_not_found():
         "owner": "IBMUSER",
         },
     )
+    assert "errors" in str(not_found_result.result)
     assert not_found_result.result["return_codes"] == keyring_not_found_return_codes
 
+def test_extract_keyring_missing_admin_type():
+    """This test is supposed to fail"""
+    not_found_result = sear(
+        {
+        "operation": "extract", 
+        "keyring": "SEARNOTFOUND",
+        "owner": "IBMUSER",
+        },
+    )
+    assert "errors" in str(not_found_result.result)
+    assert not_found_result.result["return_codes"] != keyring_not_found_return_codes
+
+def test_extract_keyring_missing_operation():
+    """This test is supposed to fail"""
+    not_found_result = sear(
+        {
+        "admin_type": "keyring", 
+        "keyring": "SEARNOTFOUND",
+        "owner": "IBMUSER",
+        },
+    )
+    assert "errors" in str(not_found_result.result)
+    assert not_found_result.result["return_codes"] != keyring_not_found_return_codes
+
 def test_extract_keyring(create_keyring):
+    """This test is supposed to succeed"""
     keyring, owner = create_keyring
     extract_result = sear(
         {
@@ -26,9 +53,11 @@ def test_extract_keyring(create_keyring):
         "owner": owner,
         },
     )
+    assert "errors" not in str(extract_result.result)
     assert extract_result.result["return_codes"] == successful_return_codes
 
 def test_add_keyring(delete_keyring):
+    """This test is supposed to succeed"""
     keyring, owner = delete_keyring
     add_result = sear(
         {
@@ -38,9 +67,11 @@ def test_add_keyring(delete_keyring):
         "owner": owner,
         },
     )
+    assert "errors" not in str(add_result.result)
     assert add_result.result["return_codes"] == successful_return_codes
 
 def test_delete_keyring(create_keyring):
+    """This test is supposed to succeed"""
     keyring, owner = create_keyring
     delete_result = sear(
         {
@@ -50,4 +81,5 @@ def test_delete_keyring(create_keyring):
         "owner": owner,
         },
     )
+    assert "errors" not in str(delete_result.result)
     assert delete_result.result["return_codes"] == successful_return_codes
